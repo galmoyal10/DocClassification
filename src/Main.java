4b38127e58ca1cfde7cab971b5cf0a8c6467e404
Yarden Moskovich <mosko169@gmail.com>
2018-06-04 00:14:06 +0300

    output file

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,93 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 
 public class Main {
 
-	private static final String QUERY_FILE_FIELD = "queryFile";
-	private static final String DOCS_FILE_FIELD = "docsFile";
-	private static final String OUT_FILE_FIELD = "outputFile";
-	private static final String MODE_FIELD = "retrievalAlgorithm";
+    private static final String QUERY_FILE_FIELD = "queryFile";
+    private static final String DOCS_FILE_FIELD = "docsFile";
+    private static final String OUT_FILE_FIELD = "outputFile";
+    private static final String MODE_FIELD = "retrievalAlgorithm";
 
-	private static final String BASIC = "basic";
-	
+    private static final String BASIC = "basic";
+    
     public static void main(String[] args) {
-    	try {    		
-    		String paramFileName = args[0];
-    		File paramFile = new File(paramFileName);
-    		FileReader fileReader = new FileReader(paramFile);
-    		BufferedReader bufferedReader = new BufferedReader(fileReader);
-    		StringBuffer stringBuffer = new StringBuffer();
-    		
-    		String queryFile = null;
-    		String docsFile = null;
-    		String outputFile = null;
-    		boolean mode = false;
-    		
-    		String line;
-    		while ((line = bufferedReader.readLine()) != null) {
-    			String[] tokens = line.split("=");
-    			switch (tokens[0]) {
-    				case QUERY_FILE_FIELD:
-    					queryFile = tokens[1];
-    					break;
-    				case DOCS_FILE_FIELD:
-    					docsFile = tokens[1];
-    					break;
-    				case OUT_FILE_FIELD:
-    					outputFile = tokens[1];
-    					break;
-    				case MODE_FIELD:
-    					mode = tokens[1].compareTo(BASIC) == 0 ? true : false;
-    					break;
-    			}
-    			stringBuffer.append(line);
-    			stringBuffer.append("\n");
-    		}
-    		fileReader.close();
+        try {           
+            String paramFileName = args[0];
+            File paramFile = new File(paramFileName);
+            FileReader fileReader = new FileReader(paramFile);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            StringBuffer stringBuffer = new StringBuffer();
+            
+            String queryFile = null;
+            String docsFile = null;
+            String outputFile = null;
+            boolean mode = false;
+            
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] tokens = line.split("=");
+                switch (tokens[0]) {
+                    case QUERY_FILE_FIELD:
+                        queryFile = tokens[1];
+                        break;
+                    case DOCS_FILE_FIELD:
+                        docsFile = tokens[1];
+                        break;
+                    case OUT_FILE_FIELD:
+                        outputFile = tokens[1];
+                        break;
+                    case MODE_FIELD:
+                        mode = tokens[1].compareTo(BASIC) == 0 ? true : false;
+                        break;
+                }
+                stringBuffer.append(line);
+                stringBuffer.append("\n");
+            }
+            fileReader.close();
 
-    		IndexingEngine ie = new IndexingEngine(docsFile, outputFile, mode);
-    		ie.run();
-    		
-    		SearchEngine se = new SearchEngine(queryFile, ie, ie.getCorpusTopTerms());
-    		TopDocs[] results = se.searchQueries();
-    		
-    		for(int i=0; i<results.length; ++i)
-    		{
-    			System.out.println("Query #" + i + ": " + results[i].totalHits + " documents found.");
-    		}
-    		
-    	} catch (Exception e) {
-    		e.printStackTrace();
-    	}
+            IndexingEngine ie = new IndexingEngine(docsFile, mode);
+            ie.run();
+            
+            SearchEngine se = new SearchEngine(queryFile, ie, ie.getCorpusTopTerms());
+            TopDocs[] results = se.searchQueries();
+            
+            
+            writeResults(outputFile, results);
+            
+            for(int i=0; i<results.length; ++i)
+            {
+                System.out.println("Query #" + i + ": " + results[i].totalHits + " documents found.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private static void writeResults(String fileName, TopDocs[] results) throws IOException {
+        File outFile = new File(fileName);
+        if (outFile.exists()) {
+            outFile.delete();
+        } else {
+            outFile.createNewFile();
+        }
+        BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
+        Integer queryId = 1;
+        for (TopDocs result: results) {
+            String resultFormat = queryId.toString();
+            for (ScoreDoc score: result.scoreDocs) {
+                resultFormat += " " + Integer.toString(score.doc);          
+            }
+            writer.write(resultFormat);
+            writer.newLine();
+            ++queryId;
+        }
+        writer.close();
     }
 }
--- src/SearchEngine.java
+++ src/SearchEngine.java
@@ -13,49 +13,50 @@ import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TopDocs;
 
 public class SearchEngine {
-	
-	SearchEngine(String queryFile, IndexingEngine ie, String [] stopWords) throws IOException
-	{
-		this._queryFile = queryFile;
-		CharArraySet stopWordSet = new CharArraySet(Arrays.asList(stopWords),true);
-		
-		//Standard Analyzer is the most sophisticated analyzer and contains removal of stop words
-		this._queryParser = new QueryParser(LuceneConstants.CONTENTS, new StandardAnalyzer(stopWordSet));
-		this._indexSearcher = new IndexSearcher(DirectoryReader.open(ie.getIndexDir()));
-	}
-	
-	public TopDocs[] searchQueries() throws IOException, ParseException
-	{
-		Query [] queries = this.parseQueriesFile(this._queryFile);
-		TopDocs[] results = new TopDocs[queries.length];
-		
-		for(int i=0; i<queries.length;++i)
-		{
-			// 	TODO: how to filter this to be only results above T score?
-			// TODO: Is this tf-idf?
-			results[i] = this._indexSearcher.search(queries[i], LuceneConstants.MAX_SEARCH);
-		}
-		
-		return results;
-	}
-	
-	
-	private Query[] parseQueriesFile(String queriesFilePath) throws IOException, ParseException {
-		String queriesString = new String(Files.readAllBytes(Paths.get(queriesFilePath)));
-		String[] queries = queriesString.split(QUERY_REGEX);
-		//remove first empty string as a result of split
-		queries = Arrays.copyOfRange(queries, 1, queries.length);
-		
-		Query[] searchQueries = new Query [queries.length];
-		for(int i=0; i<queries.length; ++i)
-		{
-			searchQueries[i] = _queryParser.parse(queries[i]);
-		}
-		return searchQueries;
-	}
-	
-	private IndexSearcher _indexSearcher;
-	private QueryParser _queryParser;
-	private String _queryFile;
+    
+    SearchEngine(String queryFile, IndexingEngine ie, String [] stopWords) throws IOException
+    {
+        this._queryFile = queryFile;
+        CharArraySet stopWordSet = new CharArraySet(Arrays.asList(stopWords),true);
+        
+        //Standard Analyzer is the most sophisticated analyzer and contains removal of stop words
+        this._queryParser = new QueryParser(LuceneConstants.CONTENTS, new StandardAnalyzer(stopWordSet));
+        this._indexSearcher = new IndexSearcher(DirectoryReader.open(ie.getIndexDir()));
+    }
+    
+    public TopDocs[] searchQueries() throws IOException, ParseException
+    {
+        Query [] queries = this.parseQueriesFile(this._queryFile);
+        TopDocs[] results = new TopDocs[queries.length];
+        
+        for(int i=0; i<queries.length;++i)
+        {
+            // tf-idf scoring - 
+            // https://lucene.apache.org/core/3_5_0/scoring.html#Scoring
+            // https://lucene.apache.org/core/3_5_0/api/core/org/apache/lucene/search/Similarity.html
+            results[i] = this._indexSearcher.search(queries[i], LuceneConstants.MAX_SEARCH);
+        }
+        
+        return results;
+    }
+    
+    
+    private Query[] parseQueriesFile(String queriesFilePath) throws IOException, ParseException {
+        String queriesString = new String(Files.readAllBytes(Paths.get(queriesFilePath)));
+        String[] queries = queriesString.split(QUERY_REGEX);
+        //remove first empty string as a result of split
+        queries = Arrays.copyOfRange(queries, 1, queries.length);
+        
+        Query[] searchQueries = new Query [queries.length];
+        for(int i=0; i<queries.length; ++i)
+        {
+            searchQueries[i] = _queryParser.parse(queries[i]);
+        }
+        return searchQueries;
+    }
+    
+    private IndexSearcher _indexSearcher;
+    private QueryParser _queryParser;
+    private String _queryFile;
     private static final String QUERY_REGEX = "\\*FIND\\s*\\d+.*";
 }
