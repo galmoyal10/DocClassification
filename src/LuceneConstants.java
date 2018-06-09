dc348049902b8352289b3a2ca30b50554bb542d9
Yarden Moskovich <mosko169@gmail.com>
2018-06-09 23:29:18 +0300

    redundant consts

--- src/LuceneConstants.java
+++ src/LuceneConstants.java
@@ -1,7 +1,5 @@
 public class LuceneConstants {
    public static final String CONTENTS = "contents";
-   public static final String FILE_NAME = "filename";
-   public static final String FILE_PATH = "filepath";
    public static final int MAX_SEARCH = 10;
    public static final double IMPROVED_RELEVANCE_THRESHOLD = 13.25;
    public static final double BASIC_RELEVANCE_THRESHOLD = 1.15;
